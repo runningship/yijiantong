5dcb01dcf2c4edc2a5edcd8d78f24e0783dc8aab
wlshi <dell-1@WLShi-PC>
2016-05-17 21:13:42 +0800

    修改了验证码接口的用户名，密码。

--- src/main/java/com/houyi/management/util/BcloudSMSSender.java
+++ src/main/java/com/houyi/management/util/BcloudSMSSender.java
@@ -16,13 +16,13 @@ import org.apache.commons.httpclient.methods.GetMethod;
 public class BcloudSMSSender {
 
 	public static void main(String[] args){
-		doSendVerifyCode("15856985122" , "1232fdsf");
+		doSendVerifyCode("13053093595" , "1232fdsf");
 	}
 	
 	public static String doSendVerifyCode(String tel , String code){
 		String url = "http://222.73.117.156/msg/";// 应用地址
-		String account = "jiekou-clcs-02";// 账号
-		String pswd = "Tch20160202";// 密码
+		String account = "houyikeji";// 账号
+		String pswd = "Tch103193";// 密码
 		String msg = "亲爱的用户，您的验证码是"+code+"，5分钟内有效。";// 短信内容
 		boolean needstatus = true;// 是否需要状态报告，需要true，不需要false
 		String product = null;// 产品ID
